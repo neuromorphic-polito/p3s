f9ec746496016d9e151ab52543ac60dfe41c178f
Evelina Forno <evelina.forno@polito.it>
2019-07-24 15:43:07 +0200

    Add info headers

--- activate.sh
+++ activate.sh
@@ -1,5 +1,27 @@
 #!/usr/bin/env bash
+
+# ==========================================================================
+#                                  P3S
+# ==========================================================================
+# This file is part of P3S.
+#
+# P3S is Free Software: you can redistribute it and/or modify it
+# under the terms found in the LICENSE[.md|.rst] file distributed
+# together with this file.
+#
+# P3S is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+# ==========================================================================
+# Author: Francesco Barchi <francesco.barchi@polito.it>
+# Author: Evelina Forno <evelina.forno@polito.it>
+# ==========================================================================
+# activate.sh: shell script to activate the virtual environment for running
+#			   MPI-enabled programs on SpiNNaker
+# ==========================================================================
+
 source ./.pyvenv/bin/activate
-cd ./spinnaker_tools
+cd ./spinnaker_P3Ss
 source ./setup
 cd -
--- install.py
+++ install.py
@@ -1,3 +1,24 @@
+# ==========================================================================
+#                                  P3S
+# ==========================================================================
+# This file is part of P3S.
+#
+# P3S is Free Software: you can redistribute it and/or modify it
+# under the terms found in the LICENSE[.md|.rst] file distributed
+# together with this file.
+#
+# P3S is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+# ==========================================================================
+# Author: Francesco Barchi <francesco.barchi@polito.it>
+# Author: Evelina Forno <evelina.forno@polito.it>
+# ==========================================================================
+# install.py: downloads and installs all the required dependencies for
+#             running programs using the MPI programming model on SpiNNaker
+# ==========================================================================
+
 from __future__ import print_function
 
 try:
